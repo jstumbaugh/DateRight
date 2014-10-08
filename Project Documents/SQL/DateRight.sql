ed1c9c623df562a1075725762be09c708ce92b74
justinTrantham <jtrantham@smu.edu>
2014-10-08 16:06:18 -0500

    SQL file and ER Diagram for DateRight

---
+++ Project Documents/SQL/DateRight.sql
@@ -0,0 +1,219 @@
+--
+-- Database: `DateRight`
+--
+CREATE DATABASE IF NOT EXISTS DateRight;
+USE DateRight;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+
+CREATE TABLE IF NOT EXISTS `Users` (
+  `UserID` int NOT NULL AUTO_INCREMENT,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Password` varchar(64) DEFAULT NULL,
+  `PasswordSalt` varchar(50) DEFAULT NULL,
+  `UserType` int NOT NULL,
+  `Sex` int NOT NULL,
+  PRIMARY KEY (`UserID`)
+) ENGINE=InnoDB;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Activities`
+--
+
+CREATE TABLE IF NOT EXISTS `Activities` (
+  `ActivityID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Cost` double DEFAULT NULL,
+  `Location` varchar(200) NOT NULL,
+  PRIMARY KEY (`ActivityID`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ActivitiesAttendees`
+--
+
+CREATE TABLE IF NOT EXISTS `ActivitiesAttendees` (
+  `ActivitiesAttendeesID` int NOT NULL AUTO_INCREMENT,
+  `ActivityID` int NOT NULL,
+  `UserID` int NOT NULL,
+  CONSTRAINT FOREIGN KEY (`ActivityID`) REFERENCES `Activities` (`ActivityID`),
+  CONSTRAINT FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`),
+  PRIMARY KEY (`ActivitiesAttendeesID`)
+) ENGINE=InnoDB;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ActivityReviews`
+--
+
+CREATE TABLE IF NOT EXISTS `ActivityReviews` (
+  `ReviewID` int NOT NULL AUTO_INCREMENT,
+  `Rating` int NOT NULL,
+  `UserID` int NOT NULL,
+  `ActivityID` int NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Cost` double DEFAULT NULL,
+  `Location` varchar(200) NOT NULL,
+  `Attended` BOOLEAN NOT NULL,
+  `ReviewTime` datetime NOT NULL,
+  CONSTRAINT FOREIGN KEY (`ActivityID`) REFERENCES `Activities` (`ActivityID`),
+  CONSTRAINT FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`),
+  PRIMARY KEY (`ReviewID`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tags`
+--
+
+CREATE TABLE IF NOT EXISTS `Tags` (
+  `TagID` int NOT NULL AUTO_INCREMENT,
+  `TagName` varchar(50) NOT NULL,
+  PRIMARY KEY (`TagID`)
+) ENGINE=InnoDB;
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tagged Activities`
+--
+
+CREATE TABLE IF NOT EXISTS `TaggedActivities` (
+  `TagID` int NOT NULL AUTO_INCREMENT,
+  `ActivityID` int NOT NULL,
+  CONSTRAINT FOREIGN KEY (`TagID`) REFERENCES `Tags` (`TagID`),
+  CONSTRAINT FOREIGN KEY (`ActivityID`) REFERENCES `Activities` (`ActivityID`),
+  PRIMARY KEY (`TagID`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `DatePlans`
+--
+
+CREATE TABLE IF NOT EXISTS `DatePlans` (
+  `DatePlanID` int NOT NULL AUTO_INCREMENT,
+  `Name` varchar(50) NOT NULL,
+  `Public` BOOLEAN NOT NULL,
+  `CreatorID` int NOT NULL,
+  `ModID` int NOT NULL,
+  `Timestamp` datetime NOT NULL,
+  CONSTRAINT FOREIGN KEY (`CreatorID`) REFERENCES `Users` (`UserID`),
+  CONSTRAINT FOREIGN KEY (`ModID`) REFERENCES `Users` (`UserID`),
+  PRIMARY KEY (`DatePlanID`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `Date Plan Reviews`
+-- 
+-- 
+
+CREATE TABLE IF NOT EXISTS `DatePlanReviews` (
+  `ReviewID` int NOT NULL AUTO_INCREMENT,
+  `Rating` int NOT NULL,
+  `Attended` BOOLEAN NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `DatePlanID` int NOT NULL ,
+  `TagID` int NOT NULL,
+  `ReviewTime` datetime NOT NULL,
+  CONSTRAINT FOREIGN KEY (`TagID`) REFERENCES `Tags` (`TagID`),
+   CONSTRAINT FOREIGN KEY (`DatePlanID`) REFERENCES `DatePlans` (`DatePlanID`),
+  PRIMARY KEY (`ReviewID`)
+) ENGINE=InnoDB;
+
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `Date Plan Attendees`
+-- 
+-- 
+
+CREATE TABLE IF NOT EXISTS `DatePlanAttendees` (
+  `DatePlanAttendeesID` int NOT NULL AUTO_INCREMENT,
+  `DatePlanID` int NOT NULL,
+  `UserID` int NOT NULL,
+  CONSTRAINT FOREIGN KEY (`DatePlanID`) REFERENCES `DatePlans` (`DatePlanID`),
+  CONSTRAINT FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`),
+  PRIMARY KEY (`DatePlanAttendeesID`)
+) ENGINE=InnoDB;
+
+
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `Date Activities`
+-- 
+-- 
+
+CREATE TABLE IF NOT EXISTS `DateActivities` (
+  `DateActivitiesID` int NOT NULL AUTO_INCREMENT,
+  `DatePlanID` int NOT NULL,
+  `ActivityID` int NOT NULL,
+  CONSTRAINT FOREIGN KEY (`DatePlanID`) REFERENCES `DatePlans` (`DatePlanID`),
+  CONSTRAINT FOREIGN KEY (`ActivityID`) REFERENCES `Activities` (`ActivityID`),
+  PRIMARY KEY (`DateActivitiesID`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `Date Plan Activity Reviews`
+-- 
+-- 
+
+CREATE TABLE IF NOT EXISTS `DatePlanActivityReviews` (
+  `DatePlanActivityReviewID` int NOT NULL AUTO_INCREMENT,
+  `ActivityReviewID` int NOT NULL,
+  `DatePlanReviewsID` int NOT NULL,
+  CONSTRAINT FOREIGN KEY (`ActivityReviewID`) REFERENCES `ActivityReviews` (`ReviewID`),
+   CONSTRAINT FOREIGN KEY (`DatePlanReviewsID`) REFERENCES `DatePlanReviews` (`ReviewID`),
+  PRIMARY KEY (`DatePlanActivityReviewID`)
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `Favorites`
+--
+
+CREATE TABLE IF NOT EXISTS `Favorites` (
+  `FavoriteID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL,
+  `ActivityID` int NOT NULL,
+  PRIMARY KEY (`FavoriteID`),
+  CONSTRAINT FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`)
+    ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT FOREIGN KEY (`ActivityID`) REFERENCES `Activities` (`ActivityID`)
+    ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+-- --------------------------------------------------------
+--
+-- Table structure for table `Reports`
+--
+
+CREATE TABLE IF NOT EXISTS `Reports` (
+  `ReportID` int NOT NULL AUTO_INCREMENT,
+  `UserID` int NOT NULL,
+  `Description` varchar(200) NOT NULL,
+  `Type` int NOT NULL,
+  CONSTRAINT FOREIGN KEY (`UserID`) REFERENCES `Users` (`UserID`),
+  PRIMARY KEY (`ReportID`)
+) ENGINE=InnoDB;
