912631acddbe7252cb6942c8c53fcad785b6db7b
JustinKon <jkonersmann@smu.edu>
2014-10-20 17:05:52 -0500

    created javascript and css for profile page

---
+++ js/profile.js
@@ -0,0 +1,19 @@
+/**
+    Author: Konersmann
+    Created: October 20, 2014
+    Maintenance Log:
+
+**/
+
+var profile = {};
+var profileData = {"userID":"0"};
+
+$.ajax({
+    url: 'api/getProfile',
+    async: false, 
+    dataType: 'json',
+    data: JSON.stringify(profileData),
+    success: function(response) {
+        profile = response;
+    }
+});
