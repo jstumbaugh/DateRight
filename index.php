72fc554875031971599508b9e21e1f039f28e313
WilsonWillson <Wilsonjc@smu.edu>
2014-10-20 15:58:59 -0500

    Started Login Page and added to .gitignore
    Login page looks a little familiar  everyone. Ignore it for right now.

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<HTML lang = "en">
+   <head>
+      <title>
+         DateRight Login
+      </title>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="js/jquery.cookie.js"></script>
+    <meta name="description" content="DateRight">
+    <meta name="keywords" content="DateRight, Date, DatePlan, login, create"> 
+    <link href="css/index.css" rel="stylesheet">
+   </head>
+<body>
+<p id="greeting"></p>
+<button id="createbut" class="accountb" onclick="createAccount()">Create Account</button>
+<button id="loginbut" class="accountb" onclick="loginfunction()">Login</button>
+<div id="fade"></div>
+<a id="make" href="orders.html">Make a Random DatePlan</a>
+  
+
+<dialog id = "createAccount">
+ <form id = "createAccountForm" onsubmit="accountSubmit()">
+    <button type="button" id="close" onclick="closeWindow()">X</button>
+   <ol> 
+    <li> <label for = "fName">First Name:</label><input type = "text" id="fNameAccount" fName = "fName" required="required"/> </li>
+    <li> <label for = "lName">Last Name:</label><input type = "text" id="lNameAccount" password = "lName" required="required"/> </li>
+    <li> <label for = "email">Email:</label><input type = "text" id="emailAccount" email = "email" required="required" pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+(?:[A-Z]{2}|com|org|net|edu|gov|mil|biz|info|mobi|name|aero|asia|jobs|museum)\b"/> </li>
+    <li> <label for = "password">Password:</label><input type = "password" id="passwordAccount" password = "password" required="required"/> </li>
+    <li> <label for = "repeatPassword">Repeat Password:</label><input type = "password" id="passwordAccount" repeatPassword = "repeatPassword" required="required"/> </li>
+    <li> <label for = "creditProvider">Credit Card Provider:</label><input type = "text" id="creditProviderAccount" password = "creditProvider"/> </li>
+    <li> <label for = "creditCardNumber">Credit Card Number:</label><input type = "text" id="creditCardNumberAccount" password = "creditCardNumber" required="required" 
+    pattern="^4[0-9]{12}(?:[0-9]{3})?|[1-5][0-9]{14}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(?:2131|1800|35\d{3})\d{11}$"> </li>
+    </ol>
+    <button type="submit" id="submitaccount" onclick="createAccount()">Submit</button>
+   </form>
+  </dialog>
+
+<dialog id = "login">
+ <form id = "loginForm" onsubmit="loginSubmit()">
+   <button type="button" id="close" onclick="closeWindow()">X</button>
+   <ol>
+    <li> <label for = "email">Email:</label><input type = "text" id="emailLogin" email = "email" required="required"/> </li>
+    <li> <label for = "password">Password:</label><input type = "password" id="passwordLogin" password = "password" required="required"/> </li>
+    </ol>
+    <button type="submit" id="submitlogin">Submit</button>
+   </form>
+  </dialog>
+
+ <script src="js/index.js"></script>
+
+</body>
+</HTML>
+
+
+
+
+
---
+++ js/index.js
@@ -0,0 +1,134 @@
+var x;
+var fadeBackground;
+
+//The Dialog boxes
+function loginfunction() {
+    x = document.getElementById("login");
+    x.open = true;
+    fadeBackground = document.getElementById("fade");
+	fadeBackground.classList.add("fadeIn");
+}
+
+function createAccount(){
+    x = document.getElementById("createAccount");
+    x.open = true;
+    fadeBackground = document.getElementById("fade");
+	fadeBackground.classList.add("fadeIn");
+}
+
+//Close the Dialog Boxes
+function closeWindow() { 
+	x.open = false;
+	fadeBackground.className = "";
+} 
+
+//How to login to our website
+function loginSubmit() {
+	event.preventDefault(); 
+	var user = new Object();
+	user.email = $("#emailLogin").val()
+	user.password = $("#passwordLogin").val()
+	console.log(user);
+
+	$.ajax({
+		type: 'POST',
+		url: 'api/index.php/login',
+		async: false,
+		content: 'application/json',
+		data: JSON.stringify(user),
+		success: function(data){
+			console.log(data);
+			var obj = JSON.parse(data);
+			if(obj.error == true) {
+				$("#loginModal").css({"border":"2px solid red"});
+				$(".errorMessage").text("silly, your login information is not correct");
+			}
+			else {
+				console.log("loading cookie");
+				/* Store user information in cookie */ 
+				$.cookie.json = true;
+				$.cookie("data", data);	
+				console.log($.cookie("data"))
+			}
+		}
+	});
+
+	document.getElementById("loginForm").reset();
+	closeWindow(); 
+
+	var info = $.cookie("data");
+	if (info.indexOf("email") >= 0)
+	{
+		document.getElementById("createbut").setAttribute("class", "inv");
+		document.getElementById("loginbut").setAttribute("id", "logout");
+		document.getElementById("logout").innerHTML = "Logout";
+		document.getElementById("logout").setAttribute("onclick", "");
+		document.getElementById("greeting").innerHTML = "Hello, " + JSON.parse(info).fName;
+	}
+}
+
+//How to create an account on our website
+function accountSubmit() {
+	event.preventDefault();		
+	console.log("It Worked!!!");
+	var user = new Object();
+	user.fName = $("#fNameAccount").val();
+	user.lName= $("#lNameAccount").val();
+	user.email = $("#emailAccount").val(); 
+	user.phonenumber = $("#phoneNumberAccount").val();
+	user.password = $("#passwordAccount").val();
+	user.creditProvider = $("#creditProviderAccount").val();
+	user.creditCardNumber = $("#creditCardNumberAccount").val(); 
+
+	$.ajax({
+		type: 'POST',
+		url: 'api/index.php/createAccount',
+		content: 'application/jsonajax',
+		data: JSON.stringify(user),
+		success: function(data){
+			console.log(data);
+		}
+	});
+
+	document.getElementById("createAccountForm").reset();
+	closeWindow(); 
+}
+
+$(document).on("click", "#logout", function(){
+		event.preventDefault();		
+		console.log("Logging out!");
+
+		$.ajax({
+			type: 'POST',
+			url: 'api/index.php/logout',
+			content: 'application/jsonajax',
+			success: function(){
+				console.log("You've been logged out!");
+			}
+		});
+
+		document.getElementById("createbut").setAttribute("class", "accountb");
+		document.getElementById("logout").innerHTML = "Login";
+		document.getElementById("logout").setAttribute("onclick", "loginfunction()");
+		document.getElementById("logout").setAttribute("id", "loginbut");
+		document.getElementById("loginbut").setAttribute("class", "accountb");
+		document.getElementById("greeting").innerHTML = "";
+});
+
+//POST REQUEST TEST CODE
+	// var user = new Object();
+	// user.email = "test@test.edu";
+	// user.password = "password";
+	// user.fName = "Testy";
+	// user.lName = "Testerson";
+	// user.creditCardNumber = "1234123412341234";
+	// user.creditProvider = "Visa";
+	// 	$.ajax({
+	// 		type: 'POST',
+	// 		url: 'api/createAccount',
+	// 		content: 'application/json',
+	// 		data: JSON.stringify(user),
+	// 		success: function(data){
+	// 			console.log(data);
+	// 		}
+	// 	});
