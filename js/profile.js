3423d53aee79b1ffd78e5c56d481dd38930e0ee8
JustinKon <jkonersmann@smu.edu>
2014-10-27 21:30:56 -0500

    added profile structure to page, fiddled with ajax call. Cannot recieve positive results from API calls.

--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,24 @@
-/**
+1/**
     Author: Konersmann
     Created: October 20, 2014
     Maintenance Log:
+        10/27/2014 - still testing with input to recieve the correct data in viewProfile call
 
 **/
 $.cookie.json = true;
 
 var profile = {};
-var profileData = {"userID":"0"};
+var profileData = {"userID":"1"};
 
 $.ajax({
-    url: 'api/getProfile',
+    url: 'api/viewProfile',
     async: false, 
     dataType: 'json',
     data: JSON.stringify(profileData),
     success: function(response) {
+        console.log(response);
         profile = response;
     }
 });
+
+console.log(profile);
