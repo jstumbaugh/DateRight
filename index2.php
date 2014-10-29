98f4619b7017bceab7472a6fbd18b5661826e490
justinTrantham <jtrantham@smu.edu>
2014-10-28 22:45:43 -0500

    Redesigned homepage
    -Everything labeled 2 after it.
    -index2.php
    -index2.js
    -index2.css
    —Added semi transparent background image to homepage

---
+++ index2.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<HTML lang = "en">
+    <head>
+        <title>
+            DateRight Login
+        </title>
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+        <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+        <script src="js/jquery.cookie.js"></script>
+        <meta name="description" content="DateRight">
+        <meta name="keywords" content="DateRight, Date, DatePlan, login, create"> 
+        <link href="css/index2.css" rel="stylesheet">
+    </head>
+<body>
+<img src="img/background.png" class="bg">
+<p id="greeting"></p>
+
+<div class="midSection">
+<img src="img/Logo.png">
+    <section id="searchBox">
+        <form id="searchbar">
+            <input type="text" name="datesearch" id="searchBoxText"  placeholder="Search..." />
+            <input type="submit" value="Search" id="searchButton" class="searchButton">
+        </form>
+        <div id="searchResults"></div>
+    </section>
+</div>
+<div id="loginButtons" class="midSection">
+    <a href="#" id="loginButton" class="button">Login </a>
+    <a href="#" id="createAccountButton" class="button">Create Account!</a>
+</div>
+<section id="loginBox" class="overlayBox">
+    <h2>Login</h2>
+    <form method="post" class="minimal" id="login">
+        <label for="email">
+            Username:
+            <input type="email" name="email" id="emailLogin" placeholder="Email address" title="Valid email address required" required />
+        </label>
+        <label for="password">
+            Password:
+            <input type="password" name="password" id="passwordLogin" placeholder="Password" pattern=".{8,}" title="At least 8 characters" required />
+        </label>
+        <button type="submit" class="btn-minimal">Sign in</button>
+    </form>
+</section>
+<section id="createAccountBox" class="overlayBox">
+    <h2>Create Account</h2>
+    <form method="post" class="minimal" id="createAccount">
+        <label for="fName">
+            First:
+            <input type="text" name="fNameAccount" id="fNameAccount" pattern="[a-zA-Z]+" title="Letters only" placeholder="First Name" required />
+        </label>
+        <label for="lName">
+            Last:
+            <input type="text" name="lNameAccount" id="lNameAccount" pattern="[a-zA-Z]+" title="Letters only" placeholder="Last Name" required />
+        </label>
+        <label for = "email">
+            Email:
+        </label>
+        <input type = "text" id="emailAccount" email = "email" required="required" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" />
+        <label for="password">
+            Password:
+            <input type="password" name="password" id="passwordAccount" placeholder="Password must be at least 8 characters" pattern=".{8,}" required />
+        </label>
+        <button type="submit" class="btn-minimal">Create Account!</button>
+    </form>
+</section>
+<script src="js/index2.js"></script>
+</body>
+</HTML>
+
+
+
+
+
---
+++ js/index2.js
@@ -0,0 +1,109 @@
+/*DDR Software */
+function init() {
+$("#loginBox").hide();
+$("#createAccountBox").hide();
+$("#loginButton").click(function(){
+        
+	$("#createAccountBox").hide();
+	$("#loginBox").show();
+        
+}); 
+$("#createAccountButton").click(function(){
+        
+    $("#createAccountBox").show();
+    $("#loginBox").hide();
+        
+}); 
+
+$('#createAccount').submit(function (event) {
+    $("#loginBox").hide();
+    $("#createAccountBox").show();
+    event.preventDefault(); 
+    var user = new Object();
+    user.fName = $("#fNameAccount").val();
+    user.lName = $("#lNameAccount").val();
+    user.email = $("#emailAccount").val();
+    user.password = $("#passwordAccount").val();
+    user.userType = 0;
+    user.sex = 0;
+    console.log(JSON.stringify(user));
+    $.ajax({
+       type: "POST",
+        url: 'api/createAccount',
+        content: 'application/json',
+        data: JSON.stringify(user),
+        success: function(data){
+            console.log(data);
+        }
+    });
+}); 
+
+$('#searchbar').submit(function (e) {
+e.preventDefault();
+var searchString = $("#searchBoxText").val();
+$.ajax({
+    type: "POST",
+    url: './api/index.php/searchActivities',
+    data: $(this).serialize(),
+    success: function(response) {
+        console.log(response);
+        var obj = $.parseJSON(response);
+        var resultDiv = "<div >"+"Search Results:"+"</div>";
+        $(resultDiv).appendTo("#searchResults");
+        if(response==500)
+            $("<div >"+"No results found"+"</div>").appendTo("#searchResults");
+        else{
+            for ( var i = 0; i < obj['results'].length; i++) {
+                var a = obj['results'][i];
+                var div_data = "<div >"+"Name: "+a['Name']+"------- Description: "+a['Description']+"</div>";
+                $(div_data).appendTo("#searchResults");
+                
+                }
+            }
+        }
+    });
+}); 
+
+$("#login").submit(function(event) {
+    event.preventDefault(); 
+    var user = new Object();
+    user.email = $("#emailLogin").val();
+    user.password = $("#passwordLogin").val();
+    console.log(JSON.stringify(user));
+    $.ajax({
+       type: "POST",
+        url: 'api/login',
+        content: 'application/json',
+        data: JSON.stringify(user),
+        success: function(data){
+            console.log(data);
+            var obj = JSON.parse(data);
+            if(obj.error == true) {
+                $("#loginModal").css({"border":"2px solid red"});
+                $(".errorMessage").text("silly, your login information is not correct");
+            }
+            else {
+                console.log("loading cookie");
+                /* Store user information in cookie */ 
+                $.cookie.json = true;
+                $.cookie("data", data); 
+                console.log($.cookie("data"))
+            }
+        }
+    });
+
+    // var info = $.cookie("data");
+    // if (info.indexOf("email") >= 0)
+    // {
+    //     //HERE"S WHERE WE LINK TO ANOTHER PAGE
+    //     self.location='search.html'
+    //     document.getElementById("createbut").setAttribute("class", "inv");
+    //     document.getElementById("loginbut").setAttribute("id", "logout");
+    //     document.getElementById("logout").innerHTML = "Logout";
+    //     document.getElementById("logout").setAttribute("onclick", "");
+    //     document.getElementById("greeting").innerHTML = "Hello, " + JSON.parse(info).fName;
+    // }
+
+    });
+}
+window.onload = init;
