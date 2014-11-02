d4953a90d328dcde00e184cc71111dfe9be86360
Adrian Hernandez <blackjackguy24@gmail.com>
2014-11-02 07:03:11 -0600

    search for activities by name

--- js/search.js
+++ js/search.js
@@ -2,11 +2,20 @@ jQuery(document).ready(function() {
 	//Search Activities
 	$('#searchform').submit(function (e) {
 		e.preventDefault();
-		var searchString = new Object();
-		searchString.tagName = $("#searchbar").val();
-		searchString = 'api/index.php/getTaggedActivities?tagName=' + searchString.tagName;
-		$('.activity').remove();
-		getActivitiesByTag(searchString);
+		if ($("input[name=activitySearch]:checked").val() == "Tag"){
+			var searchString = new Object();
+			searchString.tagName = $("#searchbar").val();
+			searchString = 'api/index.php/getTaggedActivities?tagName=' + searchString.tagName;
+			$('.activity').remove();
+			getActivitiesByTag(searchString);
+		} else if ($("input[name=activitySearch]:checked").val() == "Name"){
+			$('.activity').remove();
+			var searchString = new Object();
+			searchString.tagName = $("#searchbar").val();
+			searchString.tagName = searchString.tagName.replace(/ /g, '+');
+			searchString = 'datesearch=' + searchString.tagName;
+			getActivitiesByName(searchString);
+		}
 	});
 
 	document.captureEvents(Event.CLICK);
@@ -29,7 +38,6 @@ function getActivitiesByTag(searchString){
 	    success: function(data) {
 	    	var actData = jQuery.parseJSON(data);
 	    	var activitiesDiv = $("#activities");
-	    	console.log(user);
 			$.ajax({
 				type: 'POST',
 				url: 'api/index.php/viewFavorites',
@@ -37,7 +45,6 @@ function getActivitiesByTag(searchString){
 				data: JSON.stringify(user),
 				success: function(data2){
 					var favData = jQuery.parseJSON(data2);
-					console.log(favData.length);
 					for (i = 0; i < actData.length; i++){
 			    		var elem = "<div class='activity' id=" + actData[i].ActivityID + "></div>"
 			    		var activityDiv = $(elem).appendTo(activitiesDiv);
@@ -47,12 +54,10 @@ function getActivitiesByTag(searchString){
 			    			if (favData[j].Name == actData[i].Name){
 			    				starunstar = 'starred';
 			    				break;
-			    			}
-			    			else
+			    			} else
 			    				starunstar = 'unstarred';
 			    		}
 			    		var starString = "<p class='" + starunstar +"'></p>";
-			    		console.log(starString);
 			    		$(starString).appendTo(activityDiv);
 			    	}
 				}
@@ -67,7 +72,6 @@ function getActivitiesByTag(searchString){
 
 function getMousePosition(e){
 	favStar = document.elementFromPoint(event.clientX, event.clientY);
-	console.log(favStar);
 	if (favStar.classList.contains("starred"))
 	{
 		console.log('Currently cannor unfavorite things, sorry.');
@@ -83,7 +87,6 @@ function getMousePosition(e){
 			content: 'application/json',
 			data: JSON.stringify(tempUser),
 			success: function(data){
-				console.log(data);
 				favStar.setAttribute("class", "starred");
 			},
 			error: function(){
@@ -102,7 +105,6 @@ function getUserID(){
         async: false,
         url: 'api/index.php/getSessionInfo',
         success: function(data){
-            //console.log(data);
             sessionData = JSON.parse(data);
         }
     });
@@ -128,6 +130,43 @@ function getFavoriteActivities(){
 		},
 		error: function(){
 			console.log('Unable to get favorite activities');
+		}
+	});
+}
+
+function getActivitiesByName(searchString){
+	$.ajax({
+		type: "GET",
+	    url: 'api/index.php/searchActivities',
+	    data: searchString,
+	    success: function(data) {
+	    	var activitiesDiv = $("#activities");
+	    	var actData = $.parseJSON(data).results;
+	    	console.log(data);
+			$.ajax({
+				type: 'POST',
+				url: 'api/index.php/viewFavorites',
+				content: 'application/json',
+				data: JSON.stringify(user),
+				success: function(data2){
+					var favData = jQuery.parseJSON(data2);
+					for (i = 0; i < actData.length; i++){
+			    		var elem = "<div class='activity' id=" + actData[i].ActivityID + "></div>"
+			    		var activityDiv = $(elem).appendTo(activitiesDiv);
+			    		var starunstar;
+			    		$("<h3></h3>").text(actData[i].Name).appendTo(activityDiv);
+			    		for (j = 0; j < favData.length; j++){
+			    			if (favData[j].Name == actData[i].Name){
+			    				starunstar = 'starred';
+			    				break;
+			    			} else
+			    				starunstar = 'unstarred';
+			    		}
+			    		var starString = "<p class='" + starunstar +"'></p>";
+			    		$(starString).appendTo(activityDiv);
+			    	}
+				}
+			});
 		}
 	});
 }
--- js/testSearch.js
+++ js/testSearch.js
@@ -1,29 +1,30 @@
 jQuery(document).ready(function() {
 
-//Search Activities
-$('#searchbar').submit(function (e) {
-e.preventDefault();
-var searchString = $("#searchString").val();
-$.ajax({
-	type: "POST",
-    url: 'api/index.php/searchActivities',
-    data: $(this).serialize(),
-    success: function(response) {
-    	var obj = $.parseJSON(response);
-		var resultDiv = "<div >"+"Search Results:"+"</div>";
- 		$(resultDiv).appendTo("#searchResults");
- 		if(response==500)
- 			$("<div >"+"No results found"+"</div>").appendTo("#searchResults");
- 		else {
-			for ( var i = 0; i < obj['results'].length; i++) {
-				var a = obj['results'][i];
-				var div_data = "<div >"+"Name: "+a['Name']+"------- Description: "+a['Description']+"</div>";
-		 		$(div_data).appendTo("#searchResults");
-				
+	//Search Activities
+	$('#searchbar').submit(function (e) {
+		e.preventDefault();
+		var searchString = $("#searchString").val();
+		$.ajax({
+			type: "GET",
+		    url: 'api/index.php/searchActivities',
+		    data: console.log($(this)),
+		    success: function(data) {
+		    	console.log(this);
+		    	var obj = $.parseJSON(data);
+				var resultDiv = "<div >"+"Search Results:"+"</div>";
+		 		$(resultDiv).appendTo("#searchResults");
+		 		if(data==500)
+		 			$("<div >"+"No results found"+"</div>").appendTo("#searchResults");
+		 		else {
+					for ( var i = 0; i < obj['results'].length; i++) {
+						var a = obj['results'][i];
+						var div_data = "<div >"+"Name: "+a['Name']+"------- Description: "+a['Description']+"</div>";
+				 		$(div_data).appendTo("#searchResults");
+						
+					}
 				}
 			}
-}
-});
-});
+		});
+	});
 });
 
