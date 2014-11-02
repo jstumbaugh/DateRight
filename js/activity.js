370fdf1e27982e0c1b27219003704ff4aaf037fd
JustinKon <jkonersmann@smu.edu>
2014-11-01 20:38:15 -0500

    added submit functionality to the activities dialog, but submit sends data through url for some reason

--- js/activity.js
+++ js/activity.js
@@ -4,7 +4,28 @@
 */
 
 $(document).ready(function(){
-    $('#activityModalDialog').load('html/activity.html', function(){
-        alert('activity loaded')
-    })
+    $.ajax({
+        url: 'html/activity.html',
+        async: false,
+        dataType: 'html',
+        success: function(data) {
+            $("body").prepend(data);
+        }
+    });
+    $("div #createActivityForm").submit(createNewActivity);
 })
+
+function createNewActivity(event){
+    event.preventDefault();
+    console.log("stuff");
+
+    name = $("#nameInput").attr("value");
+    description = $("#descriptionInput").attr("value");
+    rawCost = $("#costInput").attr("value");
+    //cost = rawCost.toFixed(2);
+    location = $("#locationInput").attr("value");
+    console.log(name);
+    console.log(description);
+    console.log(cost);
+    console.log(location);
+}
