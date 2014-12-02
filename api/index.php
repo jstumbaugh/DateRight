a83cac3abc59cf5778e162069cc3cd593da6ccb5
justinTrantham <jtrantham@smu.edu>
2014-12-02 10:48:17 -0600

    API DOC STANDARD

--- api/index.php
+++ api/index.php
@@ -4,6 +4,23 @@
  * This file contains the api for all our php functions.
  */
 
+//EXAMPLE FOR API DOC:
+ /**
+  * A summary informing the user what the associated element does.
+  *
+  * A *description*, that can span multiple lines, to go _in-depth_ into the details of this element
+  * and to provide some background information or textual references.
+  *
+  * @param string $myArgument With a *description* of this argument, these may also
+  *    span multiple lines.
+  *
+  * @return void
+  */
+
+//
+
+
+ 
 //Includes
 require 'config.php';
 \Slim\Slim::registerAutoloader();
