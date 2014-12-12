34bee73afcaab2ef1666353c90333c7dd9430578
JustinKon <jkonersmann@smu.edu>
2014-12-12 02:26:30 -0600

    Added action to user search menu. It is a little orange smiling person.

--- android/DateRight/gen/com/cse3345/dateright/R.java
+++ android/DateRight/gen/com/cse3345/dateright/R.java
@@ -22,10 +22,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int ic_action_profile=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo=0x7f020002;
     }
     public static final class id {
+        public static final int action_profile=0x7f080024;
         public static final int action_settings=0x7f080023;
         public static final int createAccountButton=0x7f08001c;
         public static final int createAccountInputEmail=0x7f080012;
@@ -107,6 +109,9 @@ public final class R {
         /**  User Profile Strings 
          */
         public static final int profile=0x7f05001b;
+        /**  Menu 
+         */
+        public static final int profileMenuItem=0x7f050024;
         public static final int random=0x7f050006;
         public static final int randomDesc=0x7f050009;
         public static final int randomName=0x7f050007;
